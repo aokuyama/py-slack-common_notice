1466c7dc064be1b88846ec4313de67d6981b9552
aokuyama <ao980s@gmail.com>
2021-12-31 08:33:23 +0900

    add slack notice

---
+++ py_slack-common-notice/slack.py
@@ -0,0 +1,90 @@
+import requests
+import json
+
+
+class SlackNotice:
+    def __init__(self, content={}) -> None:
+        self.set(**content)
+
+    def set(self, url=None, level=None, title=None, text=None):
+        self.url = url
+        self.level = level
+        self.title = title
+        self.text = text
+        return self
+
+    def send(self):
+        if not self.url:
+            raise
+        level = self.getLevel(self.level)
+        color = self.getColor(level)
+        text = self.text
+        main_text = ''
+        if level > 1:
+            main_text = '<!channel>'
+        data = {
+            "text": main_text,
+            "attachments": [
+                {
+                    "title": self.title,
+                    "text": text,
+                    "color": color,
+                }
+            ]
+        }
+        return requests.post(self.url, json.dumps(data))
+
+    def getLevel(self, var):
+        if var == None:
+            return 0
+        if type(var) == int:
+            return var
+        if type(var) != str:
+            return 9
+        if var == 'notice':
+            return 1
+        if var == 'warn':
+            return 2
+        if var == 'error':
+            return 3
+        return 9
+
+    def getColor(self, level):
+        if level == 0:
+            return None
+        if level == 1:
+            return "good"
+        if level == 2:
+            return "warning"
+        if level == 3:
+            return "danger"
+        return "danger"
+
+
+if __name__ == '__main__':
+    import unittest
+
+    class SlackNoticeTest(unittest.TestCase):
+        def setUp(self):
+            self.s = SlackNotice()
+
+        def test通知レベル(self):
+            self.assertEqual(1, self.s.getLevel(1))
+            self.assertEqual(3, self.s.getLevel(3))
+            self.assertEqual(1, self.s.getLevel("notice"))
+            self.assertEqual(2, self.s.getLevel("warn"))
+            self.assertEqual(3, self.s.getLevel("error"))
+            self.assertEqual(0, self.s.getLevel(None))
+
+        def test色(self):
+            self.assertEqual("good", self.s.getColor(1))
+            self.assertEqual("danger", self.s.getColor(99))
+            self.assertEqual("warning", self.s.getColor(2))
+            self.assertIsNone(self.s.getColor(0))
+
+        def testDict(self):
+            self.s.set(**{"url": "example.com"})
+            self.assertEqual("example.com", self.s.url)
+            self.assertIsNone(self.s.level)
+
+    unittest.main()
