89df36b7c477becae7c7fe9c6292b343d8f6869d
aokuyama <ao980s@gmail.com>
2022-03-06 08:15:22 +0900

    packaging

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="slack_common_notice",
+    version="0.0.1",
+    author="aokuyama",
+    author_email="mail@aokuyama.work",
+    description="slack common notice",
+    long_description="slack common notice", ,
+    long_description_content_type="text/markdown",
+    url="https://web.aokuyama.work",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
